61a5236e05bc723da0983dcd22b93e53519bce2e
Phils Garcia <eivanphils@gmail.com>
2017-01-02 11:51:54 -0400

    Added FOSUserBundle and DoctrineFixturesBundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,38 @@
+<?php
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User extends BaseUser
+{
+    /**
+    * @ORM\Id
+    * @ORM\Column(type="integer")
+    * @ORM\GeneratedValue(strategy="AUTO")
+    */
+    protected $id;
+
+    /**
+     * @var string $first_name
+     * @ORM\Column(name="first_name", type="string", length=255, nullable=true)
+     */
+    protected $first_name;
+
+    /**
+     * @var string $last_name
+     * @ORM\Column(name="last_name", type="string", length=255, nullable=true)
+     */
+    protected $last_name;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
