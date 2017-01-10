bc10e06b799fa8a5b08b148ea15a2beeeddb398b
Phils Garcia <eivanphils@gmail.com>
2017-01-10 09:53:21 -0400

    Added home view

---
+++ src/AppBundle/Controller/HomepageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use AppBundle\Entity\User;
+
+class HomepageController extends Controller
+{
+    /**
+     * @Route("/home", name="homepage")
+     * @return Response
+     */
+    public function indexAction()
+    {
+        return $this->render('homepage/index.html.twig');
+    }
+    
+}
