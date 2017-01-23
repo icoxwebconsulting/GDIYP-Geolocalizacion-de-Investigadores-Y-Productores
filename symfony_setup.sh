b99eb78cd3692bbead73eae5fe4506ec98b0767f
Phils Garcia <eivanphils@gmail.com>
2017-01-23 14:10:41 -0400

    Updated symfony setup sh file

--- symfony_setup.sh
+++ symfony_setup.sh
@@ -15,24 +15,23 @@ if [ ! -f "composer.phar" ]; then
     curl -s http://getcomposer.org/installer
 fi
 
-if [ "$1" == "prod" ]; then
+if [ "$1" = "prod" ]
+then
     echo "Prepping for prod:"
 
     # Install dependencies
     echo "Running composer install..."
-    composer.phar install --no-dev --optimize-autoloader
+    composer install --no-dev --optimize-autoloader
 
     echo "Clearing cache..."
     app/console cache:clear --env=prod
 
     echo "Installing assets..."
     app/console assets:install --env=prod
-    app/console assetic:dump --env=prod
 else
     echo "Refreshing for dev..."
     /usr/bin/php app/console cache:clear
     /usr/bin/php app/console assets:install --symlink
-    /usr/bin/php app/console assetic:dump
 fi
 
 # make sure everything is writeable
