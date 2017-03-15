0a13859108fa6d6c7f28adf2efe511a74bc599a9
Phils Garcia <eivanphils@gmail.com>
2017-03-15 15:47:02 -0400

    Added redirect after confirm account

--- src/AppBundle/Controller/User/RegistrationController.php
+++ src/AppBundle/Controller/User/RegistrationController.php
@@ -65,4 +65,28 @@ class RegistrationController extends BaseController
         $this->get('security.token_storage')->setToken($token);
         $this->get('session')->set('_security_main', serialize($token));
     }
+
+    /**
+     * @param $token
+     * @Route("/confirm/{token}", name="registration_confirm")
+     * @return response
+     */
+    public function confirmAction($token)
+    {
+        $user = $this->container->get('fos_user.user_manager')->findUserByConfirmationToken($token);
+        $router = $this->container->get('router');
+        if (null === $user) {
+            return new RedirectResponse($router->generate('dashboard'));
+        }
+
+        $user->setConfirmationToken(null);
+        $user->setEnabled(true);
+        $user->setLastLogin(new \DateTime());
+
+        $this->container->get('fos_user.user_manager')->updateUser($user);
+        $response = new RedirectResponse($this->container->get('router')->generate('fos_user_registration_confirmed'));
+        $this->authenticateUser($user, $response);
+
+        return $response;
+    }
 }
