0c7ab946199fefec43bd611cd386c2c04012a7a5
Phils Garcia <eivanphils@gmail.com>
2017-03-21 16:36:44 -0400

    Modified infobox

--- web/assets/spotter/external/_infobox.js
+++ web/assets/spotter/external/_infobox.js
@@ -1,19 +1,17 @@
-function drawInfobox(category, infoboxContent, json, i){
-
-    if(json[i].color)          { var color = json[i].color }
-        else                        { color = '' }
-    if( json[i].price )        { var price = '<div class="price">' + json[i].price +  '</div>' }
-        else                        { price = '' }
+function drawInfobox(url, infoboxContent, json, i){
+    var color = '';
+    if(json[i].user.roles.includes("ROLE_INVESTIGATOR")) { color = 'red'; }
+    if(json[i].user.roles.includes("ROLE_PRODUCER")) { color = 'blue'; }
+    // if( json[i].price )        { var price = '<div class="price">' + json[i].price +  '</div>' }
+    //     else                        { price = '' }
     if(json[i].id)             { var id = json[i].id }
         else                        { id = '' }
-    if(json[i].url)            { var url = json[i].url }
-        else                        { url = '' }
-    if(json[i].type)           { var type = json[i].type }
-        else                        { type = '' }
-    if(json[i].title)          { var title = json[i].title }
-        else                        { title = '' }
-    if(json[i].location)       { var location = json[i].location }
-        else                        { location = '' }
+    // if(json[i].type)           { var type = json[i].type }
+    //     else                        { type = '' }
+    // if(json[i].title)          { var title = json[i].title }
+    //     else                        { title = '' }
+    // if(json[i].location)       { var location = json[i].location }
+    //     else                        { location = '' }
     // if(json[i].gallery[0])     { var gallery = json[i].gallery[0] }
     //     else                        { gallery[0] = '../img/default-item.jpg' }
 
@@ -22,7 +20,6 @@ function drawInfobox(category, infoboxContent, json, i){
     '<div class="infobox ' + color + '">' +
         '<div class="inner">' +
             '<div class="image">' +
-                '<div class="item-specific">' + drawItemSpecific(category, json, i) + '</div>' +
                 '<div class="overlay">' +
                     '<div class="wrapper">' +
                         '<a href="#" class="quick-view" data-toggle="modal" data-target="#modal" id="' + id + '">Quick View</a>' +
@@ -32,13 +29,12 @@ function drawInfobox(category, infoboxContent, json, i){
                 '</div>' +
                 '<a href="' + url +  '" class="description">' +
                     '<div class="meta">' +
-                        price +
-                        '<h2>' + title +  '</h2>' +
-                        '<figure>' + location +  '</figure>' +
+                        '<h2>' + json[i].user.firstName +  ' ' + json[i].user.lastName +  '</h2>' +
+                        '<figure>' + json[i].address.address.substr(0,30) +  '</figure>' +
                         '<i class="fa fa-angle-right"></i>' +
                     '</div>' +
                 '</a>' +
-                // '<img src="' + gallery +  '">' +
+                '<img src="../uploads/user/profile/' + json[i].user.imageName +  '">' +
             '</div>' +
         '</div>' +
     '</div>';
