c692dbd11af2703f91666b40a9f58e3b90cc3bb7
Milagros Aristigueta <aristigueta@gmail.com>
2017-04-25 07:53:48 -0400

    Added new tables and data models

---
+++ src/AppBundle/Form/ProductiveUndertakingType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Entity\Category;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class CategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add(
+                'name',
+                TextType::class
+            )
+            ->add(
+                'parent',
+                EntityType::class,
+                array(
+                    'class' => 'AppBundle:Category',
+                    'choice_label' => 'name',
+                    'attr' => array(
+                        "class" => "form-control"
+                    )
+                )
+            );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Category'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'appbundle_category';
+    }
+}
