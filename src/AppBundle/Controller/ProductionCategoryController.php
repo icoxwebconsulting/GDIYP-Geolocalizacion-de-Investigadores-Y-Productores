9c2cdd3dc2b8c5c1431a8bf201ecbab696d77dee
Milagros Aristigueta <maristig7@yahoo.com>
2017-04-27 07:24:52 -0400

    Created two controllers for dependent drop down list of Production Type and Production Category

---
+++ src/AppBundle/Controller/ProductionCategoryController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ *@Route("/production_category")
+ */
+class ProductionCategoryController extends Controller
+{
+    /**
+     * @Route("/", name="production_category_list")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $entities = $em->getRepository("AppBundle:ProductionCategory")->findAll();
+        $serializedEntity = $this->container->get('fos_js_routing.serializer')->serialize($entities, 'json');
+
+        return new Response($serializedEntity);
+    }
+}
