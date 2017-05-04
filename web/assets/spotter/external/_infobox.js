ea78568e39b024b949f59d56c47a8261751d1f68
Milagros Aristigueta <aristigueta@gmail.com>
2017-05-03 22:03:06 -0400

    Disabled temporarily 'go to details' for practices on front-end

--- web/assets/spotter/external/_infobox.js
+++ web/assets/spotter/external/_infobox.js
@@ -1,6 +1,6 @@
 function drawInfobox(url, infoboxContent, json, i){
     var color = '';
-    if(json[i].user.roles.includes("ROLE_INVESTIGATOR")) { color = 'red'; tiltle = 'Investigator'; }
+    if(json[i].user.roles.includes("ROLE_INVESTIGATOR")) { color = 'red'; title = 'Investigator'; }
     if(json[i].user.roles.includes("ROLE_PRODUCER")) { color = 'blue'; title = 'Producer'; }
     // if( json[i].price )        { var price = '<div class="price">' + json[i].price +  '</div>' }
     //     else                        { price = '' }
@@ -16,28 +16,56 @@ function drawInfobox(url, infoboxContent, json, i){
         else                        { photo = '/assets/spotter/img/items/1.jpg' }
 
     var ibContent = '';
-    ibContent =
-    '<div class="infobox ' + color + '">' +
-        '<div class="inner">' +
-            '<div class="image">' +
-                '<div class="overlay">' +
-                    '<div class="wrapper">' +
-                        // '<a href="#" class="quick-view" data-toggle="modal" data-target="#modal" id="' + id + '">Quick View</a>' +
-                        // '<hr>' +
-                        '<a href="' + url +  '" class="detail">Ir al detalle</a>' +
+    if(json[i].user.roles.includes("ROLE_INVESTIGATOR")) {
+        ibContent =
+        '<div class="infobox ' + color + '">' +
+            '<div class="inner">' +
+                '<div class="image">' +
+                    '<div class="overlay">' +
+                        '<div class="wrapper">' +
+                            // '<a href="#" class="quick-view" data-toggle="modal" data-target="#modal" id="' + id + '">Quick View</a>' +
+                            // '<hr>' +
+                            '<a href="' + url +  '" class="detail">Ir al detalle</a>' +
+                        '</div>' +
                     '</div>' +
+                    '<a href="' + url +  '" class="description">' +
+                        '<div class="meta">' +
+                            '<h2>' + json[i].user.firstName +  ' ' + json[i].user.lastName +  '</h2>' +
+                            '<figure>' + json[i].address.address.substr(0,30) +  '</figure>' +
+                            '<i class="fa fa-angle-right"></i>' +
+                        '</div>' +
+                    '</a>' +
+                    '<img src="' + photo +  '">' +
                 '</div>' +
-                '<a href="' + url +  '" class="description">' +
-                    '<div class="meta">' +
-                        '<h2>' + json[i].user.firstName +  ' ' + json[i].user.lastName +  '</h2>' +
-                        '<figure>' + json[i].address.address.substr(0,30) +  '</figure>' +
-                        '<i class="fa fa-angle-right"></i>' +
-                    '</div>' +
-                '</a>' +
-                '<img src="' + photo +  '">' +
             '</div>' +
-        '</div>' +
-    '</div>';
-
+        '</div>';
+    }
+    
+    if(json[i].user.roles.includes("ROLE_PRODUCER")) {
+        ibContent =
+        '<div class="infobox ' + color + '">' +
+            '<div class="inner">' +
+                '<div class="image">' +
+                    '<div class="overlay">' +
+                        '<div class="wrapper">' +
+                            // '<a href="#" class="quick-view" data-toggle="modal" data-target="#modal" id="' + id + '">Quick View</a>' +
+                            // '<hr>' +
+                            //'<a href="' + url +  '" class="detail">Ir al detalle</a>' +
+                        '</div>' +
+                    '</div>' +
+                    //'<a href="' + url +  '" class="description">' +
+                    '<a href="#" class="description">' +
+                        '<div class="meta">' +
+                            '<h2>' + json[i].user.firstName +  ' ' + json[i].user.lastName +  '</h2>' +
+                            '<figure>' + json[i].address.address.substr(0,30) +  '</figure>' +
+                            //'<i class="fa fa-angle-right"></i>' +
+                        '</div>' +
+                    '</a>' +
+                    '<img src="' + photo +  '">' +
+                '</div>' +
+            '</div>' +
+        '</div>';
+    }
+    
     return ibContent;
 }
