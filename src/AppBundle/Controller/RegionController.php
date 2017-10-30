d3d32fd51d453f4641dc71bb46e243867ab4ae84
Nestor Sanchez <sanchez91nestor@gmail.com>
2017-10-30 13:55:04 -0400

    refactor homepage & keep open left bar

--- src/AppBundle/Controller/RegionController.php
+++ src/AppBundle/Controller/RegionController.php
@@ -2,28 +2,27 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Region;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 /**
  *@Route("/region")
  */
 class RegionController extends Controller
 {
+
     /**
-     * @param $id
-     * @Route("/{id}", name="region_list")
-     * @return response
+     * @Route("/{id}", name="region_list", options={"expose"=true})
      */
     public function indexAction($id)
     {
         $em = $this->getDoctrine()->getManager();
         $entities = $em->getRepository("AppBundle:Region")->findBy(array('country'=>$id));
-        $serializedEntity = $this->container->get('fos_js_routing.serializer')->serialize($entities, 'json');
-
-        return new Response($serializedEntity);
+        return JsonResponse::create( $entities , 200)->setSharedMaxAge(300);
     }
 }
